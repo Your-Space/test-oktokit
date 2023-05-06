e35997979b3dcf7aaa162875b44f16834dedd688
Yura Mandzii <tymandzii@gmail.com>
2023-05-06 23:53:58 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
-import logging
 import json
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
     return func.HttpResponse(
         json.dumps({
             'method': req.method,
